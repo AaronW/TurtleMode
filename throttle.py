342d2a1069be70e73eb0a246fe2715609e18ef1d
Aaron <aw9994@cs.ship.edu>
2011-08-23 16:46:46 -0400

    Basic code and readme.

--- throttle.py
+++ throttle.py
@@ -4,8 +4,40 @@
 throttle.py
 
 Created by Aaron on 2011-08-23
+
+>>> tc.set_session(timeout=None, alt_speed_time_enabled=1)
+>>> tc.set_session(timeout=None, alt_speed_enabled=False)
 """
+from sys import argv
+import transmissionrpc
+
+script, first, second, third = argv
+
 def main():
+    pass
+
+def throttleOn():
+"""
+Turtle-mode ON
+"""
+    tc.set_session(timeout=None, alt_speed_time_enabled=1)
+
+def throttleOff():
+    tc.set_session(timeout=None, alt_speed_enabled=False)
+
+def scanHosts():
+"""
+Scan the priority addresses to see if they're online.
+"""
+    pass
+
+def addClient(address, port):
+"""
+Add a transmission client address to remotely manage.
+"""
+    pass
+
+def removeClient():
     pass
 
 if __name__ == '__main__':
